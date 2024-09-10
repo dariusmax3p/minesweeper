92659c54567c6d6b775ad8aeb6156e76470598b0
HungTP-Play <hungtp.play@gmail.com>
2024-09-10 14:53:41 +0700

    eps10: full functionalities

--- components/minesweeper-game-board/index.tsx
+++ components/minesweeper-game-board/index.tsx
@@ -19,6 +19,9 @@ export default function MinesweeperGameBoard(props: MinesweeperGameBoardProps) {
 
   const [firstMove, setFirstMove] = useState(true);
   const [flaggedCells, setFlaggedCells] = useState<FlaggedCell[]>([]);
+  const [winLose, setWinLose] = useState(0); // win = 1, lose = -1;
+  const [seconds, setSeconds] = useState(0);
+  const [timer, setTimer] = useState<NodeJS.Timeout>();
 
   const max = useMemo(() => (ROWS > COLS ? ROWS : COLS), [ROWS, COLS]);
 
@@ -77,6 +80,8 @@ export default function MinesweeperGameBoard(props: MinesweeperGameBoardProps) {
       if (isValid(randomR, randomC)) {
         const randomIndex = indexOf(randomR, randomC);
         const randomCell = _board[randomIndex];
+        if (randomCell.isMine) continue;
+
         randomCell.isMine = true;
         _board[randomIndex] = randomCell;
 
@@ -138,11 +143,42 @@ export default function MinesweeperGameBoard(props: MinesweeperGameBoardProps) {
     return board;
   };
 
+  const checkWin = (
+    board: MinesweeperCellData[],
+    flaggedCells: FlaggedCell[]
+  ) => {
+    const mines = board.filter((c) => c.isMine);
+    const isAllMinesFlagged = mines.every((m) =>
+      flaggedCells.some((c) => c.row === m.r && c.col === m.c)
+    );
+
+    const openableCells = board.filter((c) => !c.isMine);
+    const isAllOpenableCellsOpened = openableCells.every((c) => c.isOpened);
+
+    return isAllMinesFlagged && isAllOpenableCellsOpened;
+  };
+
   const openCell = (row: number, col: number, board: MinesweeperCellData[]) => {
     let _board = [...board];
     const index = indexOf(row, col);
     const cell = _board[index];
-    if (cell.isFlagged) return _board;
+
+    if (cell.isFlagged) {
+      return {
+        winLose: 0,
+        board: _board,
+      };
+    }
+
+    if (cell.isMine) {
+      cell.isOpened = true;
+      _board[index] = cell;
+
+      return {
+        winLose: -1,
+        board: _board,
+      };
+    }
 
     cell.isOpened = true;
 
@@ -159,19 +195,43 @@ export default function MinesweeperGameBoard(props: MinesweeperGameBoardProps) {
 
     _board[index] = cell;
 
-    return _board;
+    const isWin = checkWin(_board, [...flaggedCells]);
+
+    return {
+      winLose: isWin ? 1 : 0,
+      board: _board,
+    };
+  };
+
+  const startTimer = () => {
+    let startTime = new Date().getTime();
+    const _timer = setInterval(() => {
+      const now = new Date().getTime();
+      setSeconds(Math.round((now - startTime) / 1000));
+    }, 1000); // interval every second.
+
+    setTimer(_timer);
+  };
+
+  const stopTimer = () => {
+    clearInterval(timer);
   };
 
   const onOpen = (row: number, col: number) => {
     if (firstMove) {
       let _board = genBoard(row, col);
-      _board = openCell(row, col, _board);
-
+      const { winLose, board: __board } = openCell(row, col, _board);
       setFirstMove(false);
-      setBoard(_board);
+      setBoard(__board);
+      setWinLose(winLose);
+      startTimer();
     } else {
-      const _board = openCell(row, col, [...board]);
+      const { winLose, board: _board } = openCell(row, col, [...board]);
       setBoard(_board);
+      setWinLose(winLose);
+      if (winLose != 0) {
+        stopTimer();
+      }
     }
   };
 
@@ -179,6 +239,7 @@ export default function MinesweeperGameBoard(props: MinesweeperGameBoardProps) {
     let _flaggedCells = [...flaggedCells];
     if (!isValid(row, col)) {
       return {
+        winLose: 0,
         flaggedCell: _flaggedCells,
         board,
       };
@@ -188,6 +249,7 @@ export default function MinesweeperGameBoard(props: MinesweeperGameBoardProps) {
     const cell = board[index];
     if (cell.isOpened) {
       return {
+        winLose: 0,
         flaggedCell: _flaggedCells,
         board,
       };
@@ -207,37 +269,73 @@ export default function MinesweeperGameBoard(props: MinesweeperGameBoardProps) {
       board[index] = cell;
     }
 
+    const isWin = checkWin(board, _flaggedCells);
     return {
+      winLose: isWin ? 1 : 0,
       flaggedCell: _flaggedCells,
       board,
     };
   };
 
   const onFlag = (row: number, col: number) => {
-    const { flaggedCell: _flaggedCells, board: _board } = flagCell(row, col, [
-      ...board,
-    ]);
+    const {
+      flaggedCell: _flaggedCells,
+      board: _board,
+      winLose,
+    } = flagCell(row, col, [...board]);
 
     setBoard(_board);
     setFlaggedCells(_flaggedCells);
+    setWinLose(winLose);
+    if (winLose != 0) {
+      stopTimer();
+    }
+  };
+
+  const reload = () => {
+    location.reload();
   };
 
   return (
-    <div
-      className="minesweeeper-game-board flex flex-row flex-wrap"
-      style={{ width: boardWidth }}
-    >
-      {board.map((c, index) => {
-        return (
-          <MinesweeperGameCell
-            {...c}
-            onOpen={onOpen}
-            onFlag={onFlag}
-            size={Math.round(boardWidth / max)}
-            key={index}
-          />
-        );
-      })}
+    <div className="flex flex-col justify-center items-center mb-3r">
+      <span className="text-text font-text font-mono inline-block self-start text-red-400">
+        There are {MINES - flaggedCells.length} 🇻🇳 remaining.
+      </span>
+      <span className="text-text font-text font-mono inline-block self-start">
+        Second: {seconds}
+      </span>
+      <div
+        className="minesweeeper-game-board flex flex-row flex-wrap"
+        style={{ width: boardWidth }}
+      >
+        {board.map((c, index) => {
+          return (
+            <MinesweeperGameCell
+              {...c}
+              onOpen={onOpen}
+              onFlag={onFlag}
+              size={Math.round(boardWidth / max)}
+              key={index}
+            />
+          );
+        })}
+      </div>
+      {winLose < 0 && (
+        <span
+          className="text-head1 font-head1 font-serif cursor-pointer text-red-500"
+          onClick={reload}
+        >
+          You lose!
+        </span>
+      )}
+      {winLose > 0 && (
+        <span
+          className="text-head1 font-head1 font-serif cursor-pointer text-accent-600"
+          onClick={reload}
+        >
+          You Win!
+        </span>
+      )}
     </div>
   );
 }
